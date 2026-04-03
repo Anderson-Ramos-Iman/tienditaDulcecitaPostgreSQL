792c9802b07665a187455128c5736a9ba2167166
Anderson-Ramos-Iman <anderson.ramos@tecsup.edu.pe>
2026-04-03 03:11:20 -0500

    Add render.yaml

--- app-movil/config.js
+++ app-movil/config.js
@@ -1,4 +1,4 @@
 // Cambia esta URL por la URL de tu Web Service en Render cuando lo despliegues
 // Ejemplo: 'https://tiendita-backend.onrender.com/api'
 // Para pruebas locales usa tu IP local: 'http://192.168.X.X:3001/api'
-export const API_URL = 'http://192.168.100.12:3001/api';
+export const API_URL = 'https://tienditadulcecitapostgresql.onrender.com/api';
